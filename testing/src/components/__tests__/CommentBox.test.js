4348b6fe2c8166ea7ad07779c3e38955060f1a75
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-05-30 19:26:19 +0200

    Textarea tests with jest & describe block

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -1,7 +1,6 @@
 import { mount } from "enzyme";
 import React from "react";
 import CommentBox from "../CommentBox";
-import userEvent from "@testing-library/user-event";
 
 let wrapped;
 beforeEach(() => {
@@ -14,4 +13,20 @@ afterEach(() => {
 test("has a text area and a button", () => {
   expect(wrapped.find("textarea")).toHaveLength(1);
   expect(wrapped.find("button")).toHaveLength(1);
+});
+
+describe("the text area", () => {
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", { target: { value: "new comment" } }); // it basically replaces the 'event' object in handleChange in CommentBox
+    wrapped.update(); //because setState is async
+  });
+  test("has a text area that users can type in", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment"); //we need a new textarea component
+  });
+
+  test("text area gets emptied when the input is submitted", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
 });
