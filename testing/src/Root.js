e2a3cc537868d3ba68f0eec1a648472cd1d06d1b
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-07-28 20:41:34 +0200

    Middleware checking if redux state is correct

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -4,9 +4,10 @@ import { applyMiddleware, createStore } from "redux";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
 import asyncMiddleware from "./middlewares/async";
+import stateValidator from "./middlewares/stateValidator";
 
 const Root = ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(asyncMiddleware));
+  const store = createStore(reducers, initialState, applyMiddleware(asyncMiddleware, stateValidator));
   return <Provider store={store}>{children}</Provider>;
 };
 
--- testing/src/middlewares/async.js
+++ testing/src/middlewares/async.js
@@ -3,7 +3,6 @@ export default ({ dispatch }) =>
   (next) =>
   (action) => {
     console.log(action);
-    console.log(action.payload);
     if (!action.payload || !action.payload.then) return next(action); //we assume that only promises have 'then' property
     action.payload.then(({ data }) => dispatch({ ...action, payload: data }));
   };
