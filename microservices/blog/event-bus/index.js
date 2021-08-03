e020803e323505ee3bf1ec9bdc2d46ea1e53943f
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-08-03 08:53:03 +0200

    Basic implementation of event bus

---
+++ microservices/blog/event-bus/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+  axios.post("http://localhost:4000/events", event);
+  axios.post("http://localhost:4001/events", event);
+  axios.post("http://localhost:4002/events", event);
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => console.log("Listening on 4005"));
