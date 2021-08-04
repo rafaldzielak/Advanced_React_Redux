147613b0794aa80872589e9e7e0872e6e02bdadf
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-08-04 19:17:52 +0200

    Create moderation service in the backend

--- microservices/blog/comments/index.js
+++ microservices/blog/comments/index.js
@@ -19,17 +19,28 @@ app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
   await axios.post("http://localhost:4005/events", {
     type: "CommentCreated",
-    data: { id: commentId, content, postId: req.params.id },
+    data: { id: commentId, content, postId: req.params.id, status: "pending" },
   });
   commentsByPostId[req.params.id] = comments;
   return res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Received Event: ", req.body.type);
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const comments = commentsByPostId[data.postId];
+    const comment = comments.find((comment) => comment.id === data.id);
+    comment.status = data.status;
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: { ...comment, postId: data.postId },
+    });
+  }
+
   res.send({});
 });
 
