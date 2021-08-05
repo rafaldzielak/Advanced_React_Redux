bde7e800c6b13480df5d159a876bc4a0abea9763
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-08-05 19:56:45 +0200

    Front end side for comment moderation

--- microservices/blog/client/src/CommentList.js
+++ microservices/blog/client/src/CommentList.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = Object.values(comments).map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === "approved") content = comment.content;
+    if (comment.status === "pending") content = "Comment awaiting moderation";
+    if (comment.status === "rejected") content = "Comment rejected";
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
