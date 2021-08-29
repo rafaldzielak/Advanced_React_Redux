b67da5be451d0f33a82e92591932899b5b6e3ad9
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-08-29 16:21:18 +0200

    Start working on a new project with TS

--- microservices/blog/moderation/index.js
+++ microservices/blog/moderation/index.js
@@ -17,4 +17,4 @@ app.post("/events", async (req, res) => {
   res.send({});
 });
 
-app.listen(4003, () => console.log("Listening on 4003"));
+app.listen(4003, () => console.log("Listening on 4003!!"));
---
+++ microservices/ticketing/auth/src/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.listen(3000, () => console.log("Listening on port 3000"));
