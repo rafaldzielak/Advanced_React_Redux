4e50e212712503bdb6ce1bf6b2c3686a536fcfcf
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-08-30 20:24:20 +0200

    Signup route & validation

--- microservices/ticketing/auth/src/routes/signup.ts
+++ microservices/ticketing/auth/src/routes/signup.ts
@@ -1,9 +1,24 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hi!");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid!"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (errors) return res.status(400).send(errors.array());
+    const { email, password } = req.body;
+    console.log("Creating a user...");
+    res.send({ message: "User created!" });
+  }
+);
 
 export { router as signUpRouter };
