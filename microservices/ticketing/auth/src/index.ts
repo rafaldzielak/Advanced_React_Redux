2a9f0865ed9d66357cdbf2b0d011e5f34762e470
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-08-30 20:08:26 +0200

    Create routers for paths

--- microservices/ticketing/auth/src/index.ts
+++ microservices/ticketing/auth/src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signUpRouter } from "./routes/signup";
+import { singOutRouter } from "./routes/singout";
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi!");
-});
+app.use(currentUserRouter);
+app.use(signUpRouter);
+app.use(signInRouter);
+app.use(singOutRouter);
 
 app.listen(3000, () => console.log("Listening on port 3000"));
