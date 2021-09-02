154f8e0d38e7911f63afa139d41c81cfd470dc29
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-09-02 20:20:48 +0200

    GCloud setup

--- microservices/ticketing/auth/src/index.ts
+++ microservices/ticketing/auth/src/index.ts
@@ -12,9 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res) => {
-  console.log("Incomming");
-});
 app.use(currentUserRouter);
 app.use(signUpRouter);
 app.use(signInRouter);
