d3d16e437770ca918e0365810b1846ead488e97b
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-09-05 14:08:30 +0200

    Validate request middleware

--- microservices/ticketing/auth/src/routes/signin.ts
+++ microservices/ticketing/auth/src/routes/signin.ts
@@ -1,9 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Hi!");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email is invalid"),
+    body("password").trim().notEmpty().withMessage("Please supply a password"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {}
+);
 
 export { router as signInRouter };
