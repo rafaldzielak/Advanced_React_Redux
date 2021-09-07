d968d9bb87d787837dd071c3c10fd4969aec5cb0
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-09-07 18:23:08 +0200

    Divide app & index for testing purposes

---
+++ microservices/ticketing/auth/src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/error-handler";
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signUpRouter } from "./routes/signup";
+import { singOutRouter } from "./routes/singout";
+
+const app = express();
+app.set("trust proxy", true); //for ngingx
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ signed: false, secure: true })); // signed: false for not encrypting the JWT, secure for https site only
+
+app.use(currentUserRouter);
+app.use(signUpRouter);
+app.use(signInRouter);
+app.use(singOutRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export {app}
--- microservices/ticketing/auth/src/index.ts
+++ microservices/ticketing/auth/src/index.ts
@@ -1,30 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import cookieSession from "cookie-session";
 import mongoose, { mongo } from "mongoose";
-import { NotFoundError } from "./errors/not-found-error";
-import { errorHandler } from "./middlewares/error-handler";
-import { currentUserRouter } from "./routes/current-user";
-import { signInRouter } from "./routes/signin";
-import { signUpRouter } from "./routes/signup";
-import { singOutRouter } from "./routes/singout";
-
-const app = express();
-app.set("trust proxy", true); //for ngingx
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieSession({ signed: false, secure: true })); // signed: false for not encrypting the JWT, secure for https site only
-
-app.use(currentUserRouter);
-app.use(signUpRouter);
-app.use(signInRouter);
-app.use(singOutRouter);
-
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.jwt) throw new Error("jwt env variable must be defined");
