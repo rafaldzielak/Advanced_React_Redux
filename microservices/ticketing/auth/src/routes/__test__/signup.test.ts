0caf425bbdb97b8ea99149b3eec2364497abca5d
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-09-07 19:27:00 +0200

    Some more validation test

--- microservices/ticketing/auth/src/routes/__test__/signup.test.ts
+++ microservices/ticketing/auth/src/routes/__test__/signup.test.ts
@@ -7,3 +7,33 @@ it("returns a 201 on successful signup", async () => {
     .send({ email: "rafa.dyrektorek@gmail.com", password: "password" })
     .expect(201);
 });
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "rafa.dyrektorek", password: "password" })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "rafa.dyrektorek@gmail.com", password: "pas" })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  await request(app).post("/api/users/signup").send({ email: "rafa.dyrektorek@gmail.com" }).expect(400);
+  return request(app).post("/api/users/signup").send({ password: "aksjdas" }).expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "rafa.dyrektorek@gmail.com", password: "password" })
+    .expect(201);
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "rafa.dyrektorek@gmail.com", password: "password" })
+    .expect(400);
+});
