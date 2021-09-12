c23070ab1d9aad5b37ad3ea1c53d945b3a96875b
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-09-12 11:18:05 +0200

    getInitialProps method in next.js

--- microservices/ticketing/client/pages/index.js
+++ microservices/ticketing/client/pages/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+const LandingPage = ({ color }) => {
+  console.log("I am in the component", color);
+  return <div>LandingPage</div>;
 };
 
-export default index;
+LandingPage.getInitialProps = () => {
+  console.log("I am on the server");
+  return { color: "red" };
+}; // specific to next.js - if we implement it - next.js will call this function while rendering the app on the server, any data returned from the function will be available as a prop
+
+export default LandingPage;
