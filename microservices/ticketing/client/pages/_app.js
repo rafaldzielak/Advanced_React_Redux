fbb7f52cbcc94c4503d75fa86e3d7323f88ab09c
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-09-15 20:06:59 +0200

    Execute getInitialProps for every component that has it

--- microservices/ticketing/client/pages/_app.js
+++ microservices/ticketing/client/pages/_app.js
@@ -1,10 +1,10 @@
 import "bootstrap/dist/css/bootstrap.css";
 import buildClient from "../api/build-client";
 
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <>
-      <h1>Header!</h1>
+      <h1>Header! {currentUser?.email}</h1>
       <Component {...pageProps} />
     </>
   );
@@ -14,8 +14,12 @@ AppComponent.getInitialProps = async (appContext) => {
   // specific to next.js - if we implement it - next.js will call this function while rendering the app on the server, any data returned from the function will be available as a prop
   const client = buildClient(appContext.ctx);
   const { data } = await client.get("/api/users/currentuser");
-  console.log(data);
-  return data;
+  // now we execute getInitialProps of the component and app.js
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return { pageProps, ...data };
 };
 
 export default AppComponent;
