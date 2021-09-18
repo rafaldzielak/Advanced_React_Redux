f19130573a0cedf8dc843a48392b8c6e9c9ba096
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-09-18 15:39:42 +0200

    Get ticket & test for that

--- microservices/ticketing/tickets/src/app.ts
+++ microservices/ticketing/tickets/src/app.ts
@@ -3,6 +3,7 @@ import "express-async-errors";
 import cookieSession from "cookie-session";
 import { NotFoundError, errorHandler, currentUser } from "@rdticketing/common";
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
 
 const app = express();
 app.set("trust proxy", true); //for ngingx
@@ -12,6 +13,7 @@ app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" }
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
