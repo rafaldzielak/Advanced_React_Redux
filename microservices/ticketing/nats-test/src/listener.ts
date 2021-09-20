7aa75ac6db36f4a884f145a8ee7b1734282708af
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-09-20 20:43:05 +0200

    Create queue group to run multiple listeners and divide traffic

--- microservices/ticketing/nats-test/src/listener.ts
+++ microservices/ticketing/nats-test/src/listener.ts
@@ -1,12 +1,13 @@
 import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 console.clear();
 
-const stan = nats.connect("ticketing", "123", { url: "http://localhost:4222" }); // stan is client
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), { url: "http://localhost:4222" }); // stan is client
 
 stan.on("connect", () => {
   console.log("Listener connected to nats");
 
-  const substription = stan.subscribe("ticket:created");
+  const substription = stan.subscribe("ticket:created", "order-service-queue-group"); //second argument is name of queue group
   substription.on("message", (msg: Message) => {
     const data = msg.getData();
     if (typeof data === "string") console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
