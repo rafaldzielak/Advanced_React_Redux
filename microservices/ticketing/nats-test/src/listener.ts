5f8ba8e5cfc27d1e81e57b78e4f497ee3f8181a9
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-09-20 20:53:43 +0200

    Manual ack message

--- microservices/ticketing/nats-test/src/listener.ts
+++ microservices/ticketing/nats-test/src/listener.ts
@@ -1,5 +1,6 @@
 import nats, { Message } from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { sign } from "node:crypto";
 console.clear();
 
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), { url: "http://localhost:4222" }); // stan is client
@@ -7,9 +8,12 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), { url: "h
 stan.on("connect", () => {
   console.log("Listener connected to nats");
 
-  const substription = stan.subscribe("ticket:created", "order-service-queue-group"); //second argument is name of queue group
+  const options = stan.subscriptionOptions().setManualAckMode(true); //setManualAckMode - we set the ack manually, if not ack - it will send the event to another listener
+
+  const substription = stan.subscribe("ticket:created", "order-service-queue-group", options); //second argument is name of queue group
   substription.on("message", (msg: Message) => {
     const data = msg.getData();
     if (typeof data === "string") console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    msg.ack();
   });
 });
