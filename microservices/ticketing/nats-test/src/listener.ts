da9367e98d0f0914f40b036e06d455773d171d81
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-09-29 16:20:08 +0200

    Listener class

--- microservices/ticketing/nats-test/src/listener.ts
+++ microservices/ticketing/nats-test/src/listener.ts
@@ -32,6 +32,7 @@ process.on("SIGTERM", () => stan.close()); // termination signal
 abstract class Listener {
   abstract subject: string;
   abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
   protected ackWait = 5000;
   private client: Stan;
   constructor(client: Stan) {
@@ -49,6 +50,12 @@ abstract class Listener {
     const subscription = this.client.subscribe(this.subject, this.queueGroupName, this.subscriptonOptions());
     subscription.on("message", (msg: Message) => {
       console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
     });
+  }
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === "string" ? JSON.parse(data) : JSON.parse(data.toString("utf8"));
   }
 }
