55193299c6e752399ac855285991b7126b020a06
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-01 18:30:57 +0200

    Create nats wrapper singleton and emit event when creating ticket

--- microservices/ticketing/tickets/src/index.ts
+++ microservices/ticketing/tickets/src/index.ts
@@ -1,9 +1,11 @@
 import mongoose, { mongo } from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.jwt) throw new Error("jwt env variable must be defined");
   try {
+    await natsWrapper.connect("ticketing", "alskjhdaskj", "http://nats-srv:4222");
     if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
     await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to DB");
