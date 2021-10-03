4578d44685b8609892422c5f15531705a7844e71
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-03 17:02:16 +0200

    Graceful shutdown

--- microservices/ticketing/tickets/src/index.ts
+++ microservices/ticketing/tickets/src/index.ts
@@ -6,6 +6,13 @@ const start = async () => {
   if (!process.env.jwt) throw new Error("jwt env variable must be defined");
   try {
     await natsWrapper.connect("ticketing", "alskjhdaskj", "http://nats-srv:4222");
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGNINT", () => natsWrapper.client.close()); // interupt signal
+    process.on("SIGTERM", () => natsWrapper.client.close()); // termination signal
+
     if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
     await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to DB");
