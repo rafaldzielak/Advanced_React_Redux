8e1918d3453b5bbde925584d750a1d17576ae995
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-04 17:16:03 +0200

    Nats env variables

--- microservices/ticketing/tickets/src/index.ts
+++ microservices/ticketing/tickets/src/index.ts
@@ -4,8 +4,12 @@ import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.jwt) throw new Error("jwt env variable must be defined");
+  if (!process.env.NATS_CLIENT_ID) throw new Error("NATS_CLIENT_ID must be defined");
+  if (!process.env.NATS_URL) throw new Error("NATS_URL must be defined");
+  if (!process.env.NATS_CLUSTER_ID) throw new Error("NATS_CLUSTER_ID must be defined");
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
   try {
-    await natsWrapper.connect("ticketing", "alskjhdaskj", "http://nats-srv:4222");
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed!");
       process.exit();
@@ -13,7 +17,6 @@ const start = async () => {
     process.on("SIGNINT", () => natsWrapper.client.close()); // interupt signal
     process.on("SIGTERM", () => natsWrapper.client.close()); // termination signal
 
-    if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
     await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to DB");
   } catch (error) {
