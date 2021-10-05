d0b3746245270a2e451ba1fad6f475f030768510
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-05 18:33:24 +0200

    Initial orders service setup

---
+++ microservices/ticketing/orders/src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { NotFoundError, errorHandler, currentUser } from "@rdticketing/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+app.set("trust proxy", true); //for ngingx
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })); // signed: false for not encrypting the JWT, secure for https site only
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ microservices/ticketing/orders/src/index.ts
@@ -0,0 +1,32 @@
+import mongoose, { mongo } from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  if (!process.env.jwt) throw new Error("jwt env variable must be defined");
+  if (!process.env.NATS_CLIENT_ID) throw new Error("NATS_CLIENT_ID must be defined");
+  if (!process.env.NATS_URL) throw new Error("NATS_URL must be defined");
+  if (!process.env.NATS_CLUSTER_ID) throw new Error("NATS_CLUSTER_ID must be defined");
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
+  try {
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGNINT", () => natsWrapper.client.close()); // interupt signal
+    process.on("SIGTERM", () => natsWrapper.client.close()); // termination signal
+
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("Connected to DB");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
+
+start();
+ 
