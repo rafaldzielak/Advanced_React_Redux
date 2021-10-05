a2fbdf377ab47389c676486eaa8d6e3717395dfe
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-05 18:40:03 +0200

    Basic routes setup for ordering service

--- microservices/ticketing/orders/src/app.ts
+++ microservices/ticketing/orders/src/app.ts
@@ -2,10 +2,10 @@ import express from "express";
 import "express-async-errors";
 import cookieSession from "cookie-session";
 import { NotFoundError, errorHandler, currentUser } from "@rdticketing/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes";
+import { deleteOrderRouter } from "./routes/delete";
 
 const app = express();
 app.set("trust proxy", true); //for ngingx
@@ -14,10 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })); // signed: false for not encrypting the JWT, secure for https site only
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
