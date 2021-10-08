dd7421e25462d04e5d4c8ce770fd3388b8efd256
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-08 18:56:36 +0200

    Publish order created & deleted events

--- microservices/ticketing/orders/src/routes/delete.ts
+++ microservices/ticketing/orders/src/routes/delete.ts
@@ -1,18 +1,21 @@
 import { NotAuthorizedError, NotFoundError, OrderStatus, requireAuth } from "@rdticketing/common";
 import express, { Request, Response } from "express";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
 import { Order } from "../models/order";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
 router.delete("/api/orders/:orderId", requireAuth, async (req: Request, res: Response) => {
   const { orderId } = req.params;
-  const order = await Order.findById(orderId);
+  const order = await Order.findById(orderId).populate("ticket");
   if (!order) throw new NotFoundError();
   if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
   order.status = OrderStatus.Cancelled;
   await order.save();
   res.send(order);
   // TODO: publish event saying this was cancelled
+  new OrderCancelledPublisher(natsWrapper.client).publish({ id: order.id, ticket: { id: order.ticket.id } });
 });
 
 export { router as deleteOrderRouter };
--- microservices/ticketing/orders/src/routes/new.ts
+++ microservices/ticketing/orders/src/routes/new.ts
@@ -8,8 +8,10 @@ import {
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import mongoose from "mongoose";
+import { OrderCreatedPublisher } from "../events/publishers/order-created-publisher";
 import { Order } from "../models/order";
 import { Ticket } from "../models/ticket";
+import { natsWrapper } from "../nats-wrapper";
 
 const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
@@ -43,6 +45,13 @@ router.post(
     await order.save();
     res.status(201).send(order);
     // TODO: Publish en event
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      status: order.status,
+      userId: order.userId,
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: { id: ticket.id, price: ticket.price },
+    });
   }
 );
 
