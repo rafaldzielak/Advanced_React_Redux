c49554f4dfbc32770060b48e68afa078720a230f
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-10 10:44:14 +0200

    OCC for tickets

---
+++ microservices/ticketing/tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,27 @@
+import { Ticket } from "../ticket";
+
+it("implemets optimistic concurrency control", async () => {
+  const ticket = Ticket.build({ title: "concert", price: 5, userId: "123" });
+  await ticket.save();
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+  await firstInstance!.save();
+  try {
+    await secondInstance!.save();
+  } catch (error) {
+    return;
+  }
+  throw new Error("SHould not reacth this point");
+});
+
+it("increments the version number on multiple saves", async () => {
+  const ticket = Ticket.build({ title: "concert", price: 5, userId: "123" });
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
