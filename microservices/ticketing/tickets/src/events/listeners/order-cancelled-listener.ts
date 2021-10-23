cad55d78b80c0e9d154e23da1b535d9218f7434b
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-23 11:01:16 +0200

    Order cancelled event subject fix

---
+++ microservices/ticketing/tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,13 @@
+import { Listener, OrderCancelledEvent, Subjects } from "@rdticketing/common";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+import { queueGroupName } from "./queueGroupName";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+  queueGroupName = queueGroupName;
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    throw new Error("Method not implemented.");
+  }
+}
