06f5ed29aac7b744fda8f0b1c32a62c2fbfcda20
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-23 10:53:25 +0200

    Test functionality for publishing event inside listener

--- microservices/ticketing/tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ microservices/ticketing/tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -39,3 +39,11 @@ it("acks the message", async () => {
   await listener.onMessage(data, msg);
   expect(msg.ack).toHaveBeenCalled();
 });
+
+it("publishes a ticket updated event", async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+  const ticketUpdatedData = JSON.parse((natsWrapper.client.publish as jest.Mock).mock.calls[0][1]);
+  expect(ticketUpdatedData.orderId).toEqual(data.id);
+});
