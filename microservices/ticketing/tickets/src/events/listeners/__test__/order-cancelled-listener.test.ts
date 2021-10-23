077602bc7e4d495182f3ad62758d4695bc9f4b8d
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-23 11:10:14 +0200

    Order cancelled listener & test

---
+++ microservices/ticketing/tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models/ticket";
+import { natsWrapper } from "../../../nats-wrapper";
+import { OrderCancelledEvent } from "@rdticketing/common";
+import { OrderCancelledListener } from "../order-cancelled-listener";
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+  const ticket = Ticket.build({ title: "concert", price: 99, userId: "asdf" });
+  ticket.set({ orderId });
+
+  await ticket.save();
+
+  const data: OrderCancelledEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    ticket: { id: ticket.id },
+  };
+
+  // @ts-ignore
+  const msg: Message = { ack: jest.fn() };
+
+  return { listener, ticket, data, msg, orderId };
+};
+
+it("updates the ticket, publishes an event and acks the message", async () => {
+  const { listener, data, msg, ticket } = await setup();
+  await listener.onMessage(data, msg);
+  const updatedTicket = await Ticket.findById(ticket.id);
+  expect(updatedTicket!.orderId).not.toBeDefined();
+  expect(msg.ack).toHaveBeenCalled();
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
--- microservices/ticketing/tickets/src/events/listeners/order-cancelled-listener.ts
+++ microservices/ticketing/tickets/src/events/listeners/order-cancelled-listener.ts
@@ -8,6 +8,18 @@ export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
   subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
   queueGroupName = queueGroupName;
   async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
-    throw new Error("Method not implemented.");
+    const ticket = await Ticket.findById(data.ticket.id);
+    if (!ticket) throw new Error("Ticket not found!");
+    ticket.set({ orderId: undefined });
+    await ticket.save();
+    new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    });
+    msg.ack();
   }
 }
