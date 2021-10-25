bc74a8f118b3e85fa02ddf6896cfbc0c2302f5ae
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-25 20:38:34 +0200

    15 minutes delay

--- microservices/ticketing/expiration/src/events/listeners/order-created-listener.ts
+++ microservices/ticketing/expiration/src/events/listeners/order-created-listener.ts
@@ -7,7 +7,9 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    await expirationQueue.add({ orderId: data.id });
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log("Waiting: " + delay + "ms");
+    await expirationQueue.add({ orderId: data.id }, { delay });
     msg.ack();
   }
 }
