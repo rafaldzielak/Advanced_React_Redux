3b81cb6a8c81716be0d0f8caa0264f669e55b7de
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-10-26 20:59:59 +0200

    Initial setup of payments service

---
+++ microservices/ticketing/payments/src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { NotFoundError, errorHandler, currentUser } from "@rdticketing/common";
+
+const app = express();
+app.set("trust proxy", true); //for ngingx
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })); // signed: false for not encrypting the JWT, secure for https site only
+app.use(currentUser);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+ 
+app.use(errorHandler);
+
+export { app };
