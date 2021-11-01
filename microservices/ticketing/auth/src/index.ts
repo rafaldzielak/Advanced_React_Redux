00932bab18c358c16130ee721971c0dc2ea7970f
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-11-01 08:17:47 +0100

    CI: add startup message

--- microservices/ticketing/auth/src/index.ts
+++ microservices/ticketing/auth/src/index.ts
@@ -2,6 +2,7 @@ import mongoose, { mongo } from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  console.log("Starting up...");
   if (!process.env.jwt) throw new Error("jwt env variable must be defined");
   try {
     if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
