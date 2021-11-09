8ba7f8b3a674b845ef56f2917d18c6591f88a0d5
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-11-09 08:27:41 +0100

    Book Context

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -2,15 +2,18 @@ import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeToggle from "./components/ThemeToggle";
 import AuthContextProvider from "./contexts/AuthContext";
+import BookContextProvider from "./contexts/BookContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <AuthContextProvider>
         <ThemeContextProvider>
           <Navbar />
-          <BookList />
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
           <ThemeToggle />
         </ThemeContextProvider>
       </AuthContextProvider>
--- context-api/src/components/BookList.js
+++ context-api/src/components/BookList.js
@@ -1,18 +1,20 @@
 import React, { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
+import { BookContext } from "../contexts/BookContext";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 const BookList = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
-  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+  const { books } = useContext(BookContext);
   const theme = isLightTheme ? light : dark;
 
   return (
-    <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
+    <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
       <ul>
-        <li style={{ background: theme.ui }}>The way of kings</li>
-        <li style={{ background: theme.ui }}>The name of the wind</li>
-        <li style={{ background: theme.ui }}>The final empire</li>
+        {books.map((book) => (
+          <li style={{ background: theme.ui }} key={book.id}>
+            {book.title}
+          </li>
+        ))}
       </ul>
     </div>
   );
