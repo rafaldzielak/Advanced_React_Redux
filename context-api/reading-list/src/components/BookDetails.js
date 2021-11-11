ba665015293fbeb05a99f7f6822e0c5be82475c3
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-11-11 07:49:31 +0100

    Add local storage to store the list

--- context-api/reading-list/src/components/BookDetails.js
+++ context-api/reading-list/src/components/BookDetails.js
@@ -4,6 +4,7 @@ import { BookContext } from "../contexts/BookContext";
 const BookDetails = ({ book }) => {
   const { dispatch } = useContext(BookContext);
 
+
   return (
     <li onClick={() => dispatch({ type: "REMOVE_BOOK", id: book.id })}>
       <div className='title'>{book.title}</div>
--- context-api/reading-list/src/contexts/BookContext.js
+++ context-api/reading-list/src/contexts/BookContext.js
@@ -1,10 +1,17 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 import { bookReducer } from "../reducers/BookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = ({ children }) => {
-  const [books, dispatch] = useReducer(bookReducer, []);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem("books");
+    if (localData) return JSON.parse(localData);
+    return [];
+  });
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   return <BookContext.Provider value={{ books, dispatch }}>{children}</BookContext.Provider>;
 };
